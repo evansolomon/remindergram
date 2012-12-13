c9bf8a64cb0bb3f1b58da2d3f66ad39ed190b16f
Evan Solomon <evan@evanalyze.com>
2012-12-13 00:15:44 -0800

    Add a query object for API requests

--- website/api.py
+++ website/api.py
@@ -4,18 +4,37 @@ from remindergram import source, recommendation
 
 
 def get_photos():
-    service = request.form['service']
-    identifier = request.form['identifier']
-    if not hasattr(source, service):
+    query = _Query(request)
+    if not hasattr(source, query.service):
         return jsonify({'error': 'Invalid service'})
 
-    s = getattr(source, service)(identifier)
+    s = getattr(source, query.service)(query.identifier)
     if not s.photos:
         return jsonify({'error': 'No photos'})
 
-    recs = recommendation.Recommendation(s.photos, {'days': 20, 'size': 500})
+    recs = recommendation.Recommendation(s.photos, {'days': query.days, 'size': query.size})
     photos = [rec.url for rec in recs.get()]
     if not len(photos):
         return jsonify({'error': 'No photos'})
 
     return Response(json.dumps(photos), mimetype='application/json')
+
+
+class _Query(object):
+    def __init__(self, request):
+        self.set_defaults()
+        self.parse_request(request)
+
+    def set_defaults(self):
+        self.days = 20
+        self.size = 500
+
+    def parse_request(self, request):
+        for key, val in request.form.items():
+            setattr(self, key, val)
+
+        return self
+
+    # Fallback, always be able to request a query attribute without errors
+    def __getattr__(self, attr):
+        return None
